fded3b6c928aa70418b46d43bf674ae29bdc7b3d
fatemeh <fatisati@gmail.com>
2023-04-20 21:29:46 +0430

    gene-set enrichment added

--- biological_analysis/gene_enrichment.py
+++ biological_analysis/gene_enrichment.py
@@ -1,18 +1,22 @@
 import pandas as pd
 import requests  ## python -m pip install requests
 import json
+import pickle as pkl
+import shared_names
 from biological_analysis.string_names import StringNames
 
 
 class StringApi:
-    def __init__(self):
+    def __init__(self, res_path):
         string_api_url = "https://version-11-5.string-db.org/api"
         output_format = "json"
         method = "enrichment"
         self.request_url = "/".join([string_api_url, output_format, method])
+        self.gene_name_dict = pkl.load(open(res_path + shared_names.gene_name_dic, 'rb'))
+        self.res_path = res_path
 
     def get_enrichment(self, genes, max_fdr=0.05):
-        print(f'getting string enrichment for gene-set with size:{len(genes)}...')
+        # print(f'getting string enrichment for gene-set with size:{len(genes)}...')
 
         params = {
             "identifiers": "%0d".join(genes),  # your protein
@@ -29,22 +33,29 @@ class StringApi:
             go = df[df['category'].isin(['Component', 'Process', 'Function'])]
             kegg = df[df['category'] == 'KEGG']
 
-            print('done')
+            # print('done')
             return list(kegg['term']), list(go['term'])
         except:
-            print(data)
+            # print(data)
             return [], []
+
+    def coms_gene_enrichment(self, coms):
+        go_list, kegg_list = [], []
+        for com in coms:
+            gene_names = [self.gene_name_dict[gene_id] for gene_id in com]
+            go, kegg = self.get_enrichment(gene_names)
+            go_list.append(go)
+            kegg_list.append(kegg)
+        go_cnt_list = [len(go) for go in go_list]
+        kegg_cnt_list = [len(kegg) for kegg in kegg_list]
+        df = pd.DataFrame({'com': coms, 'go': go_list, 'go-cnt': go_cnt_list,
+                           'kegg': kegg_list, 'kegg-cnt': kegg_cnt_list})
+        df.to_csv(self.res_path + 'gene-set-enrichment.csv')
+        return go_list, kegg_list
 
 
 if __name__ == '__main__':
-    # genes = ['FBN2', 'RYR2', 'C2orf88', 'ZFHX4', 'GABRB2', 'RPS6KA2', 'KCNIP1', 'OTX2-AS1', 'RP11-457K10.1', 'MAD1L1', 'EGLN3', 'KLHL29', 'COL23A1', 'MRPS22']
-    genes = ['FBN2', 'RYR2', 'C2orf88', 'ZFHX4', 'GABRB2', 'RPS6KA2', 'KCNIP1', 'OTX2-AS1', 'RP11-457K10.1', 'MAD1L1',
-             'EGLN3', 'KLHL29', 'COL23A1', 'MRPS22']  # string_name = StringNames('../../../data/')
-    # genes = [string_name.translate_gene(g) for g in genes]
-    # print(genes)
-    api = StringApi()
-    kegg, go = api.get_enrichment(genes)
-    print(len(kegg))
-    print(kegg.head())
-    # kegg.to_csv('kegg.csv')
-    # go.to_csv('go.csv')
+    res_path = '../results/'
+    coms = pkl.load(open(res_path + 'h-ensemble1000-coms0.15.pkl', 'rb'))
+    string_api = StringApi(res_path)
+    string_api.coms_gene_enrichment(coms)
